1069221d333bcd3aa546aa6007180b2e365aeeb0
Hamish Murphy <hamishmurphy@gmail.com>
2014-08-29 21:35:20 +1000

    Working with ssl

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,8 +1,8 @@
-class { 'apt':
-#  always_apt_update => true
-}
+# class { 'apt':
+# #  always_apt_update => true
+# }
 # apt::ppa { 'ppa:ondrej/php5': }
-apt::ppa { 'ppa:rip84/php5': }
+# apt::ppa { 'ppa:rip84/php5': }
 # apt::builddep { 'python-software-properties': }
 
 # Class['::apt::update'] -> Package <|
@@ -11,12 +11,35 @@ apt::ppa { 'ppa:rip84/php5': }
 # |>
 
 class { 'apache': }
+#   mpm_module => 'prefork',
+#   default_vhost => false
+# }
+include apache::ssl
+# class { 'apache::mod::ssl': }
+# class { 'apache::mod::php': }
 
 apache::vhost { 'default':
     docroot             => '/vagrant',
     server_name         => false,
-    priority            => '',
+    priority            => '000',
     template            => 'apache/virtualhost/vhost.conf.erb',
+
+    directory => '/vagrant',
+    directory_options => 'Indexes FollowSymLinks Includes ExecCGI',
+    directory_require => 'all granted'
+}
+
+apache::vhost { 'default-ssl':
+    port                => 443,
+    docroot             => '/vagrant',
+    server_name         => false,
+    priority            => '000',
+    template            => 'apache/virtualhost/test.vhost.conf.erb',
+    ssl                 => true,
+
+    directory => '/vagrant',
+    directory_options => 'Indexes FollowSymLinks Includes ExecCGI',
+    directory_require => 'all granted'
 }
 
 class { 'php': }
